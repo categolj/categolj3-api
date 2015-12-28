5878fcb8d8ba0bb7d3bd436e51fb99a720b9cc5a
Toshiaki Maki <makingx@gmail.com>
2015-12-28 14:09:29 +0900

    Add file download

---
+++ src/main/java/am/ik/categolj3/api/entry/EntryFileDownloadController.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2015 Toshiaki Maki <makingx@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package am.ik.categolj3.api.entry;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EntryFileDownloadController {
+    @Value("${downloadFile:classpath:/new.md}")
+    Resource downloadFile;
+
+    @RequestMapping(path = "new")
+    ResponseEntity<Resource> download() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.parseMediaType("text/x-markdown"))
+                .body(downloadFile);
+    }
+}
